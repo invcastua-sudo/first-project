cd1dda4baeefeb0a12f0918a5147b9397aba8a16
dmytrorepak <invcast.ua@gmai.com>
2026-06-07 20:59:41 +0100

    feat: create main window

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,5 +1,17 @@
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+
     Paddle player1;
     Paddle player2;
     Ball ball;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+    }
 }
